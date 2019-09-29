fdadb2e086bf25cc3d23f079520adb3b2dbb1ae1
Ankur <ankur171941.cse@chitkara.edu.in>
2019-09-29 14:52:01 +0530

    2 component Product list and Product details to be made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,9 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { UserCartComponent } from './user-cart/user-cart.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,10 @@ import { UserCartComponent } from './user-cart/user-cart.component';
     HomePageComponent,
     NavBarComponent,
     ProductDetailsComponent,
-    UserCartComponent
+    UserCartComponent,
+    ProductListComponent,
+    LoginComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -1,10 +1,20 @@
 import {Routes} from '@angular/router';
 import {HomePageComponent} from './home-page/home-page.component';
 import {UserCartComponent} from './user-cart/user-cart.component';
+import {ProductListComponent} from './product-list/product-list.component';
+import {ProductDetailsComponent} from './product-details/product-details.component';
+import {LoginComponent} from './login/login.component';
+import {SignUpComponent} from "./sign-up/sign-up.component";
 
 // tslint:disable-next-line:variable-name
-export const Main_Routes: Routes = [{
-  path: 'home', component: HomePageComponent,
-},
-  {path: 'cart', component: UserCartComponent, }];
+export const Main_Routes: Routes = [
+  {  path: '', redirectTo: 'home', pathMatch: 'full'},
+  {  path: 'home', component: HomePageComponent, },
+  {  path: 'login', component: LoginComponent, },
+{  path: 'SignUp', component: SignUpComponent, },
+
+  {  path: 'cart', component: UserCartComponent, },
+  {  path: 'productList', component: ProductListComponent, },
+  {  path: 'productDetails', component: ProductDetailsComponent, }
+  ];
 
