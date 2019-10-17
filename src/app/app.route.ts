3a09acf53dc19bfe8303ad6bb2b682e9f547109e
Ankur <ankur171941.cse@chitkara.edu.in>
2019-10-17 08:17:58 +0530

    another commit

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -5,6 +5,7 @@ import {UserCartComponent} from './user-cart/user-cart.component';
 import {ProductDetailsComponent} from './product-details/product-details.component';
 import {LoginComponent} from './login/login.component';
 import {SignUpComponent} from './sign-up/sign-up.component';
+import {OrderhistoryComponent} from "./orderhistory/orderhistory.component";
 
 // tslint:disable-next-line:variable-name
 export const Main_Routes: Routes = [
@@ -12,6 +13,7 @@ export const Main_Routes: Routes = [
   {  path: 'home', component: HomePageComponent, },
   {  path: 'login', component: LoginComponent, },
 {  path: 'SignUp', component: SignUpComponent, },
+{  path: 'OrderHistory', component: OrderhistoryComponentomponent, },
 
   {  path: 'cart', component: UserCartComponent, },
 
